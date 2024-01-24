ab69aa0661236036879c9a234110f13e3f7c0603
unknown <jordan@lmksystem.fr>
2024-01-24 13:33:15 +0400

    amélioration

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import './Modal.css'
+import React from "react";
+import "./Modal.css";
 
-function Modal({ isOpen, children, styleContainer, styleContent, renderClose, toggle }) {
-	return (
-		<div className='modal-container' style={{ ...styleContainer }}>
-			<div className='modal-wrapper'>
-				<div className='modal-close-btn'>
-					{renderClose ? renderClose : <div className='close-btn' onClick={toggle}>x</div>}
-				</div>
-				<div className='modal-content' style={{ ...styleContent }}>
-					{children}
-				</div>
-			</div>
-
-		</div>
-	)
+function Modal({
+  isOpen,
+  children,
+  styleContainer,
+  styleContent,
+  renderClose,
+  toggle,
+}) {
+  return (
+    isOpen && (
+      <div className="jrdnps-modal-container" style={{ ...styleContainer }}>
+        <div className="jrdnps-modal-content" style={{ ...styleContent }}>
+          {children}
+          {renderClose ? (
+            renderClose
+          ) : (
+            <div className="jrdnps-close-btn" onClick={toggle}>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  );
 }
 
-export default Modal
+export default Modal;
